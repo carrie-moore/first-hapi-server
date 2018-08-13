41121b707dce462e35245900e12cf77040a86db7
carriecarter <carrie@carrie-carter.com>
2018-08-13 15:20:24 -0700

    arg. need new instructions

---
+++ app.js
@@ -0,0 +1,41 @@
+// @flow
+
+'use strict';
+
+const Hapi = require('hapi');
+
+const port = process.env.PORT || 4000;
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 4000
+});
+
+// const routes = require('./routes');
+
+// server.route(routes);
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler(request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/api',
+    handler(request, reply) {
+        reply('Hello, API!');
+    }
+});
+
+server.start(err => {
+    if(err) {
+        throw err;
+    }
+    // console.log(`Server running at: ${server.info.uri}`);
+});
+
+exports.server = server;
